3b721a1c46c44df426d0500f90fd4394629afb55
Juan_Manuel_Trujillo <juanperdomotorre.13@gmail.com>
2023-06-28 15:24:29 -0500

    ejercicio con dart

--- Ejercicios_dart/Ejercicio_11.dart
+++ Ejercicios_dart/Ejercicio_11.dart
@@ -1,28 +1,25 @@
 import 'dart:io';
 
-void main () {
+void main() {
 /*Tres personas deciden invertir su dinero para fundar una empresa. Cada una de ellas invierte una cantidad distinta.
 Obtener el porcentaje que cada quien invierte con respecto a la cantidad total invertida*/
- // Leer la cantidad de inversión de cada persona
-  print('Ingrese la cantidad de inversión de la primera persona:');
-  double inversion1 = double.parse(stdin.readLineSync()!);
 
-  print('Ingrese la cantidad de la inversión de la segunda persona:');
-  double inversion2 = double.parse(stdin.readLineSync()!);
+  print('Ingrese el valor de la inversión de la primera persona:');
+  double inver1 = double.parse(stdin.readLineSync()!);
+  print('Ingrese el valor de la inversión de la segunda persona:');
+  double inver2 = double.parse(stdin.readLineSync()!);
+  print('Ingrese el valor de la inversión de la tercera persona:');
+  double inver3 = double.parse(stdin.readLineSync()!);
 
-  print('Ingrese la cantidad de la inversión de la tercera persona:');
-  double inversion3 = double.parse(stdin.readLineSync()!);
+  double totaldinero = inver1 + inver2 + inver3;
+  double primerporciento = (inver1 / totaldinero) * 100;
+  double segundoporciento = (inver2 / totaldinero) * 100;
+  double tercerporciento = (inver3 / totaldinero) * 100;
+  String resul1 = primerporciento.toStringAsFixed(2);
+  String resul2 = segundoporciento.toStringAsFixed(2);
+  String resul3 = tercerporciento.toStringAsFixed(2);
 
-  // Calcular la cantidad total invertida
-  double totalInvertido = inversion1 + inversion2 + inversion3;
-
-  // Calcular el porcentaje de inversión de cada persona
-  double porcentaje1 = (inversion1 / totalInvertido) * 100;
-  double porcentaje2 = (inversion2 / totalInvertido) * 100;
-  double porcentaje3 = (inversion3 / totalInvertido) * 100;
-
-  // Mostrar los porcentajes de inversión de cada persona
-  print('Porcentaje de inversión de la primera persona: $porcentaje1%');
-  print('Porcentaje de inversión de la segunda persona: $porcentaje2%');
-  print('Porcentaje de inversión de la tercera persona: $porcentaje3%');
+  print('Porcentaje de inversión de la primera persona: $resul1%');
+  print('Porcentaje de inversión de la segunda persona: $resul2%');
+  print('Porcentaje de inversión de la tercera persona: $resul3%');
 }
